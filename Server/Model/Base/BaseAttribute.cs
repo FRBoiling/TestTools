4b73059470947863610db9556003dcacf5135aca
Boil <FRee22858@live.com>
2019-04-24 18:32:23 +0800

    update

---
+++ Server/Model/Base/BaseAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model.Base
+{
+    public class BaseAttribute : Attribute
+    {
+        public Type AttributeType { get; }
+
+        public BaseAttribute()
+        {
+            this.AttributeType = this.GetType();
+        }
+    }
+}
